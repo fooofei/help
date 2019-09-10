1e6a0f1a5efbb747487b87f155558ef977d7fec4
fooofei <aihujianfei@gmail.com>
2019-09-10 20:55:59 +0800

    Update main.rs

--- qa1/src/main.rs
+++ qa1/src/main.rs
@@ -20,9 +20,16 @@ struct IterMut<'a> {
 impl<'a> Iterator for IterMut<'a> {
     type Item = &'a mut String;
     // how to return ?
+    // Please make a pull request or make a issue to help me fix this.
     fn next(&mut self) -> Option<Self::Item> {
         self.s.as_mut().map(|v| &mut (**v).s)
     }
 }
 
-fn main() {}
+fn main() {
+    let mut v1 = Some(Box::new(IString::new(String::from("a"))));
+    let iter = v1.iter_mut();
+    for v in iter {
+        println!("v={:?}", v);
+    }
+}
