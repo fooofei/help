5c5f7aaf1121434eb75268e6fc144166a1c2aaa0
Kam Y. Tse <kevin.xjy@gmail.com>
2019-09-11 00:17:13 +0800

    update main.rs
    A simple LinkedList and impl Iterator for it.

--- qa1/src/main.rs
+++ qa1/src/main.rs
@@ -1,33 +1,44 @@
+
 #[derive(Debug)]
 struct IString {
     s: String,
-    n: OBString,
+    pub n: Option<Box<IString>>,
 }
-type OBString = Option<Box<IString>>;
 
 impl IString {
     pub fn new(s: String) -> IString {
         IString { s: s, n: None }
     }
     pub fn iter_mut(&mut self) -> IterMut<'_> {
-        IterMut { s: &mut self.n }
+        IterMut { s: Some(self) }
     }
 }
 
 struct IterMut<'a> {
-    s: &'a mut OBString,
+    s: Option<&'a mut IString>,
 }
+
 impl<'a> Iterator for IterMut<'a> {
     type Item = &'a mut String;
     // how to return ?
     // Please make a pull request or make a issue to help me fix this.
     fn next(&mut self) -> Option<Self::Item> {
-        self.s.as_mut().map(|v| &mut (**v).s)
+        self.s.take().map(|s| {
+            self.s = s.n.as_mut().map(|x| &mut **x);
+            &mut s.s
+        })
     }
 }
 
 fn main() {
-    let mut v1 = Some(Box::new(IString::new(String::from("a"))));
+    let mut v1 = IString::new(String::from("a"));
+    v1.n = Some(Box::new(IString::new(String::from("b"))));
+    let iter = v1.iter_mut();
+    for v in iter {
+        println!("v={:?}", v);
+        v.push_str("__")
+    }
+    
     let iter = v1.iter_mut();
     for v in iter {
         println!("v={:?}", v);
