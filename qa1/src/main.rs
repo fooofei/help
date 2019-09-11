4b37bbb47cff8445463c52845cda07c9f06af329
fooofei <aihujianfei@gmail.com>
2019-09-11 20:21:26 +0800

    rewrite for compare

--- qa1/src/main.rs
+++ qa1/src/main.rs
@@ -1,25 +1,25 @@
 #[derive(Debug)]
-struct IString {
+struct Node {
     s: String,
-    n: OBString,
+    n: Option<Box<Node>>,
 }
-type OBString = Option<Box<IString>>;
 
-impl IString {
-    pub fn new(s: String) -> IString {
-        IString { s: s, n: None }
-    }
-    pub fn iter_mut(&mut self) -> IterMut<'_> {
-        IterMut { s: &mut self.n }
+impl Node {
+    fn new(s :String) -> Self {
+        Self{
+            s:s,
+            n:None
+        }
     }
 }
 
 // I know a way in https://rust-unofficial.github.io/too-many-lists/second-iter-mut.html
+// for differ with IterMut, we named NodeMutRef
 #[derive(Debug)]
-struct IterMut<'a> {
-    s: &'a mut OBString, //  I know Option<&'a mut IString> is ok, but why cannot this?
+struct NodeMutRef<'a> {
+    n: &'a mut Option<Box<Node>>, //  I know Option<&'a mut Node> is ok, but why cannot this?
 }
-impl<'a> Iterator for IterMut<'a> {
+impl<'a> Iterator for NodeMutRef<'a> {
     type Item = &'a mut String;
     // how to return ?
     // Please make a pull request or make a issue to help me fix this.
@@ -30,42 +30,49 @@ impl<'a> Iterator for IterMut<'a> {
     }
 }
 
-impl<'a> IterMut<'a> {
-    // 步骤拆分 // 仅仅看是否能够返回可修改借用 // 是能够做到的
-    fn test_mut(&mut self) -> Option<&mut String> {
-        let r = self.s.as_mut().map(|v| {
-            &mut (**v).s
-        });
-        return r;
+impl<'a> NodeMutRef<'a> {
+
+    fn new(l :&'a mut Option<Box<Node>>) -> Self {
+        Self{
+            n:l,
+        }
     }
 
     // 步骤拆分 // 仅仅看是否能够做到链表后移 // 做不到
     fn test_move_next(&mut self) {
         // `old_node` does not live long enough
-//        let mut old_node = self.s.take();
-//        old_node.as_mut().map(|e|{
-//            self.s = &mut e.n;
-//        });
+        let mut old_node = self.n.take();
+        old_node.as_mut().map(|e|{
+            self.n = &mut e.n;
+        });
     }
 
+    // 步骤拆分 // 仅仅看是否能够返回可修改借用 // 是能够做到的
+    fn test_mut(&mut self) -> Option<&mut String> {
+        let r = self.n.as_mut().map(|e| {
+            &mut (**e).s
+        });
+        return r;
+    }
+}
+
+
+#[test]
+fn test1(){
+    let mut head = Node::new(String::from("aaa"));
+    let tail = Box::new(Node::new(String::from("bbb")));
+    head.n = Some(tail);
+
+    let mut iter = NodeMutRef::new(&mut head.n);
+    let mut s0 = iter.test_mut();
+    s0.map(|e| *e = format!("--{}",e));
+    assert_eq!(iter.test_mut(), Some(&mut String::from("--bbb")));
+
+    assert_eq!(iter.test_mut(), Some(&mut String::from("--bbb")));
+
+    iter.test_move_next();
+    assert_eq!(iter.test_mut(), None);
 }
 
 fn main() {
-    let mut head = IString::new(String::from("aaa"));
-    let mut tail = Box::new(IString::new(String::from("bbb")));
-    head.n = Some(tail);
-
-    for v in head.iter_mut() {
-        println!("v={:?}", v);
-        *v = format!("11{:?}", *v);
-        println!("v={:?}", v);
-    }
-    println!("head={:?}", head);
-    let mut v2 = head.iter_mut();
-    v2.test_mut().map(|e| {
-        *e = format!("--{}", e);
-        println!("e={:?}", e)
-    });
-
-    println!("head={:?}", head);
 }
